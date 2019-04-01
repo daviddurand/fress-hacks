9357e57a8d5bc3fc50beaaf42ea008c2ed796e86
David G. Durand <david.durand@tizra.com>
2019-04-01 08:10:37 -0400

    update tn-train conversion script to include missing format step.

--- tn-conv.sh
+++ tn-conv.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 trans $0/../tn-train <$1 | paps --lpi=6 --cpi=10 --paper=letter --format=pdf --top-margin=10 --bottom-margin=11 --font=Menlo --trunc > $1.pdf
+paps resource.tn --lpi=6.1 --cpi=11 --paper=letter --format=pdf --top-margin=0 --bottom-margin=0 --font=Menlo --trunc > resource.pdf
+
